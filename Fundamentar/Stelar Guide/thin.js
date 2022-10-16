5be5ec14adc251c08172f74d803a73ead29a632b
Kesley Lima <kesleylimasi@gmail.com>
2022-10-15 22:24:27 -0300

    splice x slice

---
+++ Fundamentar/Stelar Guide/thin.js
@@ -0,0 +1,14 @@
+let kesley = "Kesley"
+
+console.log(kesley.split(""))
+
+
+let phrase = "Eu quero aprender a programar."
+
+console.log(phrase.includes(""))
+console.log(Array.from(phrase))
+
+let x = ['oi', 'kls', 'sanduixhe']
+
+console.log(x.slice(1, 2))
+console.log(x.splice(0, 4))
